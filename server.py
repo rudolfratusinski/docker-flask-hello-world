7e30d00e3fb5b95f0dd2db921eff4e242f1e5285
Rudolf Ratusiński <rudolf.ratusinski@gmail.com>
2019-09-30 11:57:24 +0800

    fix binding

--- server.py
+++ server.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def hello():
     return jsonify({"message": "hello, world!"})
 
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
